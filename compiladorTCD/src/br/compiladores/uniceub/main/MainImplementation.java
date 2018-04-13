199bbda215a4d5e718747da883f98ab7e5cccfab
luciano.porto <luciano.porto@AN0800624077428.dinfor.uniceub>
2018-04-13 20:37:39 -0300

    exercicio 5 ready

--- compiladorTCD/src/br/compiladores/uniceub/main/MainImplementation.java
+++ compiladorTCD/src/br/compiladores/uniceub/main/MainImplementation.java
@@ -3,14 +3,16 @@ package br.compiladores.uniceub.main;
 import br.compiladores.uniceub.exercicios.Exercicio1;
 import br.compiladores.uniceub.exercicios.Exercicio2;
 import br.compiladores.uniceub.exercicios.Exercicio4;
+import br.compiladores.uniceub.exercicios.Exercicio5;
 
 public class MainImplementation {
 
 	public static void main(String[] args) {
 
-//		Exercicio1.execute();
-//		Exercicio2.execute();
-		Exercicio4.execute();
+		// Exercicio1.execute();
+		// Exercicio2.execute();
+		// Exercicio4.execute();
+		Exercicio5.execute();
 	}
 
 }
